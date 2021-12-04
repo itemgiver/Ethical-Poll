f3577e0e3613aae9553727a0efe701f07652f669
itemgiver <williamgyuhosuh@naver.com>
2021-12-04 21:34:56 +0900

    Fix: firebase pollRef error

--- lib/utils/postPoll/index.tsx
+++ lib/utils/postPoll/index.tsx
@@ -10,13 +10,12 @@ async function PostPoll(id: number, question: string) {
     .get();
 
   if (targetPoll.docs.length === 0) {
-    const pollRef = await firebase
-      .firestore()
-      .collection(CollectionName.POLL)
-      .add({
-        id: id,
-        question: question,
-      });
+    const pollRef = await firebase.firestore().collection(CollectionName.POLL);
+
+    pollRef.add({
+      id: id,
+      question: question,
+    });
   } else {
     const pollPathId = targetPoll.docs[0].id;
     const pollRef = await firebase
